533b9004655db022b57b1b5e019aea2845dd50eb
Thomas-DL <Thomas-DL@users.noreply.github.com>
2024-04-09 09:48:57 +0000

    Fix styling

--- src/Livewire/GetPosts.php
+++ src/Livewire/GetPosts.php
@@ -8,50 +8,50 @@ use Nody\NodyBlog\Models\Post;
 
 class GetPosts extends Component
 {
-  /**
-   * The number of posts to display.
-   *
-   * @var int
-   *
-   * @param  int  $postsCount
-   */
-  public $postsCount; // Integer
+    /**
+     * The number of posts to display.
+     *
+     * @var int
+     *
+     * @param  int  $postsCount
+     */
+    public $postsCount; // Integer
 
-  /**
-   * The posts to display.
-   *
-   * @var mixed
-   *
-   * @param  mixed  $categories
-   */
-  public $categories; // Array
+    /**
+     * The posts to display.
+     *
+     * @var mixed
+     *
+     * @param  mixed  $categories
+     */
+    public $categories; // Array
 
-  /**
-   * The posts to display.
-   *
-   * @var mixed
-   *
-   * @param  mixed  $tags
-   */
-  public $tags; // Array
+    /**
+     * The posts to display.
+     *
+     * @var mixed
+     *
+     * @param  mixed  $tags
+     */
+    public $tags; // Array
 
-  /**
-   * Show load more button
-   *
-   * @var bool
-   *
-   * @param  bool  $showLoadMore
-   */
-  public $showLoadMore; // Boolean
+    /**
+     * Show load more button
+     *
+     * @var bool
+     *
+     * @param  bool  $showLoadMore
+     */
+    public $showLoadMore; // Boolean
 
-  #[Computed()]
-  public function posts()
-  {
-    return Post::published()->latest()->take($this->postsCount)->get();
-  }
+    #[Computed()]
+    public function posts()
+    {
+        return Post::published()->latest()->take($this->postsCount)->get();
+    }
 
-  public function render()
-  {
-    return view('nody-blog::livewire.get-posts');
-  }
+    public function render()
+    {
+        return view('nody-blog::livewire.get-posts');
+    }
 }
