d50d609c5e333008050afe71e973cf6f56b485b2
Thomas-DL <Thomas-DL@users.noreply.github.com>
2024-04-17 16:18:18 +0000

    Fix styling

--- resources/lang/fr/filament.php
+++ resources/lang/fr/filament.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-  "category_resource" => "Catégories",
-  "post_resource" => "Articles",
-  "tag_resource" => "Étiquettes",
-  "comment_resource" => "Commentaires",
+    'category_resource' => 'Catégories',
+    'post_resource' => 'Articles',
+    'tag_resource' => 'Étiquettes',
+    'comment_resource' => 'Commentaires',
 ];
